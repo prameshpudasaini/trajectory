eb20c202f8db2c4a0ebfbc7941fc868242ab2b64
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2022-11-28 16:27:32 -0700

    Merged zipped csv files

---
+++ script/merge_zipped_csv.py
@@ -0,0 +1,27 @@
+import os
+import zipfile
+import pandas as pd
+
+dir_name = r"D:\GitHub\trajectory\ignore\Phoenix\20221122_CSVExport"
+file_list = os.listdir(dir_name)
+
+device_list = []
+for file in file_list:
+    device_list.append(file[5:7])
+
+cols = ['TimeStamp', 'EventID', 'Parameter']
+df = []
+i = 0
+
+for file in file_list:
+    if file.endswith('.zip'):
+        zip_file = os.path.join(dir_name, file)
+        zf = zipfile.ZipFile(zip_file)
+        temp = pd.read_csv(zf.open(zf.namelist()[0]), names = cols)
+        temp['DeviceID'] = device_list[i]
+        df += [temp]
+    i += 1
+        
+output_path = dir_name + '\\' + '20221122_CSVExport' + '.txt'
+
+pd.concat(df, ignore_index = True).to_csv(output_path, index = False, sep = '\t')
