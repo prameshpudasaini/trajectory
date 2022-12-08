a3e13fd6baec407f729e40ff0d7741446e3ad699
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2022-12-07 19:21:46 -0700

    Minor update

--- script/PCD.R
+++ script/PCD.R
@@ -3,47 +3,83 @@ library(ggplot2)
 library(patchwork)
 library(plotly)
 
-param_eb <- c(17, 18, 19, 20, 37, 38, 39, 40, 61, 62, 63, 64)
-param_wb <- c(9, 10, 11, 12, 29, 30, 31, 32, 53, 54, 55, 56)
-param_nb <- c(13, 14, 15, 16, 33, 34, 35, 36, 57, 58, 59, 60)
-param_sb <- c(21, 22, 23, 24, 41, 42, 43, 44)
-
-phase_eb <- 6L
-phase_wb <- 2L
-
-det_eb_stop <- c(17L, 18L, 19L, 20L)
-det_wb_stop <- c(9L, 10L, 11L, 12L)
-det_eb_advance <- c(37L, 38L, 39L, 40L)
-det_wb_advance <- c(29L, 30L, 31L, 32L)
-
 am_peak <- c(6L, 7L, 8L)
 pm_peak <- c(15L, 16L, 17L)
 
-folder_name <- '20221201_IndianSchool'
+# detector configuration for Indian School Rd
+phase_eb <- 6L
+phase_wb <- 2L
+
+det_eb_stop <- c(17L, 18L, 19L)
+det_eb_advance <- c(20L, 25L, 26L)
+
+det_wb_stop <- c(9L, 10L, 11L, 12L)
+det_wb_advance <- c(27L, 28L, 29L)
+
+# read data
+folder_name <- '20221207_IndianSchool'
 file_name <- paste0("ignore/Phoenix/", folder_name, '.txt')
 data <- fread(file_name)
 data[, TimeStamp := as.POSIXct(TimeStamp, tz = '', format = '%m-%d-%Y %H:%M:%OS')]
 
 options(digits.secs = 3L)
 
+# convert Device ID
+convertDevID <- function(x) {
+    x[, DeviceID := factor(as.factor(fcase(DeviceID == 46, '19th Ave',
+                                           DeviceID == 47, '15th Ave',
+                                           DeviceID == 48, '7th Ave',
+                                           DeviceID == 49, '3rd Ave')),
+                           levels = c('19th Ave', '15th Ave', '7th Ave', '3rd Ave'))][]
+    return(x)
+}
+
 # check detector configuration
-DT <- copy(data)[hour(TimeStamp) %in% am_peak & Parameter %in% param_wb & EventID == 82L, ]
-DT[, .N, by = c('DeviceID', 'Parameter')][order(DeviceID, Parameter)]
+checkDetConf <- function(peak, param, dir) {
+    dt <- copy(data)[hour(TimeStamp) %in% peak & Parameter %in% param & EventID == 82L, ]
+    dt <- dt[, .N, by = c('DeviceID', 'Parameter')][order(DeviceID, Parameter)]
+    dt <- convertDevID(dt)
+    dt[, DeviceID := paste0(DeviceID, ' ', dir)][]
+    return(dt)
+}
+
+checkDetConf(am_peak, det_eb_stop, 'EB')
+checkDetConf(am_peak, det_eb_advance, 'EB')
+
+checkDetConf(am_peak, det_wb_stop, 'WB')
+checkDetConf(am_peak, det_wb_advance, 'WB')
 
 # check data continuity
-DT_eb <- copy(data)[hour(TimeStamp) %in% am_peak & Parameter %in% param_eb & EventID == 82L, ]
-DT_wb <- copy(data)[hour(TimeStamp) %in% am_peak & Parameter %in% param_wb & EventID == 82L, ]
+checkDataCont <- function(peak, det_type) {
+    
+    if (det_type == 'stop') {
+        param_eb <- det_eb_stop
+        param_wb <- det_wb_stop
+    } else {
+        param_eb <- det_eb_advance
+        param_wb <- det_wb_advance
+    }
+    
+    dt_eb <- copy(data)[hour(TimeStamp) %in% peak & Parameter %in% param_eb & EventID == 82L, ]
+    dt_eb <- convertDevID(dt_eb)
+    
+    dt_wb <- copy(data)[hour(TimeStamp) %in% peak & Parameter %in% param_wb & EventID == 82L, ]
+    dt_wb <- convertDevID(dt_wb)
+    
+    p1 <- ggplot(dt_eb) + 
+        geom_point(aes(TimeStamp, DeviceID)) + 
+        ylab('Eastbound') + 
+        theme(axis.title.x = element_blank())
+    
+    p2 <- ggplot(dt_wb) + 
+        geom_point(aes(TimeStamp, DeviceID)) + 
+        ylab('Westbound') + 
+        theme(axis.title.x = element_blank())
+    
+    return(p2 / p1)
+}
 
-p1 <- ggplot(DT_wb) + 
-    geom_point(aes(TimeStamp, DeviceID)) + 
-    theme(axis.text.x = element_blank(),
-          axis.title.x = element_blank(),
-          axis.ticks.x = element_blank())
-
-p2 <- ggplot(DT_eb) + 
-    geom_point(aes(TimeStamp, DeviceID))
-
-p1 / p2
+checkDataCont(am_peak, 'stop')
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -51,8 +87,11 @@ p1 / p2
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # process data
+device <- 49L
+hour <- 8L
+minute <- 35L
 
-DT <- copy(data)[DeviceID == 49L & hour(TimeStamp) %in% c(7L) & minute(TimeStamp) <= 50, ]
+DT <- copy(data)[DeviceID == device & hour(TimeStamp) %in% hour & minute(TimeStamp) <= minute, ]
 DT$DeviceID <- NULL
 
 DT <- DT[Parameter %in% c(phase_wb, det_wb_stop), ][order(TimeStamp)] # westbound
