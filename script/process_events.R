7b8560a967b126e886ab73ce2fcb35068b952524
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2023-01-03 19:06:15 -0700

    Updated function, saved file

--- script/process_events.R
+++ script/process_events.R
@@ -1,5 +1,4 @@
 library(data.table)
-library(plotly)
 
 # detector configuration for Indian School Rd
 phase_eb <- 6L
@@ -27,7 +26,7 @@ options(digits.secs = 3L)
 
 seq_phase_event <- c(7L, 8L, 9L, 10L, 11L, 1L)
 
-processEvents <- function(device, det_type, dir, hour, minute) {
+processEvents <- function(device, det_type, dir, hour, minute, exclude_SRT = TRUE) {
     
     # define parameters for detection type
     if (det_type == 'stop') {
@@ -55,6 +54,7 @@ processEvents <- function(device, det_type, dir, hour, minute) {
     phase <- copy(data)[EventID %in% seq_phase_event & Parameter %in% phase, ][order(TimeStamp)]
     print("Check number of signal phase events:")
     print(phase[, .N, by = EventID])
+    cat('\n')
     
     minCycleTime <- phase$TimeStamp[min(which(phase$EventID == 7L))]
     maxCycleTime <- phase$TimeStamp[max(which(phase$EventID == 7L))]
@@ -78,7 +78,9 @@ processEvents <- function(device, det_type, dir, hour, minute) {
     det <- det[between(TimeStamp, minCycleTime, maxCycleTime, incbounds = FALSE), ][order(TimeStamp)]
     
     det_dir_loc <- sort(unique(det$Parameter))
-    print(paste0('Detectors: ', det_dir_loc))
+    print("Detectors:")
+    print(det_dir_loc)
+    cat('\n')
     
     # get detection parameters for each lane-by-lane detector
     getOHG <- function(lane) {
@@ -122,7 +124,9 @@ processEvents <- function(device, det_type, dir, hour, minute) {
     DT[, GreenStart := as.POSIXct(GreenStart, origin = origin)]
     
     YellowTime <- DT$RedStart - head(DT$YellowStart, -1L) # check yellow times
-    print(YellowTime)
+    print("Yellow time:")
+    print(summary(as.numeric(head(YellowTime, -1L))))
+    cat('\n')
     
     DT[, AIC := round(as.numeric(TimeStamp - YellowStart), 3L)]
     DT[, TUG := round(as.numeric(GreenStart - TimeStamp), 3L)]
@@ -139,6 +143,7 @@ processEvents <- function(device, det_type, dir, hour, minute) {
         return(list(SSC_check = SSC_check, SSC = SSC))
     }
     
+    print("Check SSC for vector of detector on & off:")
     for (i in seq_along(det_dir_loc)) {
         print(paste0(i, ': ', det_dir_loc[i], ': ', getSSC(det_dir_loc[i])$SSC_check))
         
@@ -151,20 +156,58 @@ processEvents <- function(device, det_type, dir, hour, minute) {
     
     DT <- DT[EventID == 82L, ][order(TimeStamp)]
     
+    SSC_levels <- c('YY', 'YR', 'RR', 'RG', 'GG', 'GY')
+    DT[, SSC := factor(as.factor(SSC), levels = SSC_levels)]
+    
+    summary_SSC <- DT[, .(count = .N, 
+                          min_ODT = min(ODT),
+                          max_ODT = max(ODT),
+                          mean_ODT = round(mean(ODT), 3L),
+                          median_ODT = median(ODT), 
+                          sd_ODT = round(sd(ODT), 4L)), by = c('SSC', 'Parameter')][order(SSC, Parameter)]
+    
+    cat('\n')
+    print("Summary of signal status change for ODT:")
+    print(summary_SSC)
+    cat('\n')
+    
+    # exclude shared right-turn
+    print('Exclude shared right-turn lane?')
+    print(exclude_SRT)
+    cat('\n')
+    
+    if (exclude_SRT == TRUE) {
+        DT <- DT[Parameter != det_dir_loc[1], ]
+    }
+    
+    # set time until green to zero for GG & GY
+    DT[, TUG := fifelse(SSC %in% c('GG', 'GY'), 0L, TUG)]
+    DT[, Parameter := as.factor(Parameter)]
+    
+    DT[SSC %in% c('GG', 'GY'), ODTR := round(ODT / shift(ODT, type = 'lead'), 4L), by = .(Cycle, Parameter)]
+    DT[SSC %in% c('GG', 'GY'), GapR := round(Gap / shift(Gap, type = 'lead'), 4L), by = .(Cycle, Parameter)][]
+    
     return(DT)
 }
 
 wb_3rd_stop <- processEvents(49L, 'stop', 'WB', 8L, 35L)
+wb_3rd_stop[, Int := '3rd'][, Det := 'stop']
+
 wb_7th_stop <- processEvents(48L, 'stop', 'WB', 8L, 35L)
+wb_7th_stop[, Int := '7th'][, Det := 'stop']
+
 wb_15th_stop <- processEvents(47L, 'stop', 'WB', 8L, 35L)
-wb_19th_stop <- processEvents(46L, 'stop', 'WB', 8L, 35L)
+wb_15th_stop[, Int := '15th'][, Det := 'stop']
 
-wb_7th_advance <- processEvents(48L, 'advance', 'WB', 8L, 35L)
-wb_19th_advance <- processEvents(46L, 'advance', 'WB', 8L, 35L)
+wb_19th_stop <- processEvents(46L, 'stop', 'WB', 8L, 35L, FALSE)
+wb_19th_stop[, Int := '19th'][, Det := 'stop']
 
-# fwrite(wb_3rd_stop, 'output/20221214_ISR/wb_3rd_stop.txt')
-# fwrite(wb_7th_stop, 'output/20221214_ISR/wb_7th_stop.txt')
-# fwrite(wb_15th_stop, 'output/20221214_ISR/wb_15th_stop.txt')
-# fwrite(wb_19th_stop, 'output/20221214_ISR/wb_19th_stop.txt')
-# fwrite(wb_7th_advance, 'output/20221214_ISR/wb_7th_advance.txt')
-# fwrite(wb_19th_advance, 'output/20221214_ISR/wb_19th_advance.txt')
+wb_7th_adv <- processEvents(48L, 'advance', 'WB', 8L, 35L, FALSE)
+wb_7th_adv[, Int := '7th'][, Det := 'adv']
+
+wb_19th_adv <- processEvents(46L, 'advance', 'WB', 8L, 35L, FALSE)
+wb_19th_adv[, Int := '19th'][, Det := 'adv']
+
+DT <- rbindlist(list(wb_3rd_stop, wb_7th_stop, wb_15th_stop, wb_19th_stop, wb_7th_adv, wb_19th_adv))
+
+# fwrite(DT, "output/20221214_ISR_wb_3rd_19th.txt")
