0c9f43347406f0d0fb58198fef8b234421dd7f96
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2023-01-03 19:05:45 -0700

    Minor update

--- script/inrix.R
+++ script/inrix.R
@@ -4,25 +4,30 @@ library(plotly)
 
 source("ignore/keys.R")
 
-am_peak <- c(6L, 7L, 8L)
-pm_peak <- c(15L, 16L, 17L)
-
 db_speed <- '[ADOT_INRIX].[dbo].[Inrix_Realtime]'
 
-query <- paste0(
+seg_wb <- c('450124637', '450124638', '450124850', '450124851')
+seg_wb <- paste(sQuote(seg_wb, "'"), collapse = ',')
+seg_wb <- paste0("(", seg_wb, ")")
+
+# time: 2022-12-14 8:00 - 8:15
+query_wb <- paste0(
     "SELECT timestamp, SegmentID, speed, score, travelTimeMinutes FROM ", db_speed,
-    " WHERE SegmentID IN ('450124637', '450124638', '450124850', '450124851') AND timestamp BETWEEN '2022-11-30 00:00:00' AND '2022-12-02 00:00:00'"
+    " WHERE SegmentID IN ", seg_wb,
+    " AND timestamp BETWEEN '2022-12-14 00:00:00' AND '2022-12-14 23:59:59'"
 )
 
-DT <- as.data.table(sqlQuery(getSQLConnection('STL5'), query))
+DT <- as.data.table(sqlQuery(getSQLConnection('STL5'), query_wb))
 
 DT[, timestamp := as.POSIXct(timestamp, format = '%Y-%m-%d %H:%M:%S') - 7*3600]
-DT <- DT[wday(timestamp) == 5L & hour(timestamp) == 7L, ][order(timestamp)]
+DT <- DT[hour(timestamp) == 8L & minute(timestamp) <= 15L, ][order(timestamp)]
 
 DT[, SegmentID := fcase(SegmentID == 450124637, 'Central-3rd',
                         SegmentID == 450124638, '3rd-7th',
                         SegmentID == 450124850, '7th-15th',
                         SegmentID == 450124851, '15th-19th')]
+
+plot_ly(DT, type = 'box', y = ~speed, color = ~SegmentID, boxmean = TRUE)
 
 plot_ly(DT, type = 'scatter', x = ~timestamp, y = ~speed, color = ~SegmentID, mode = 'lines+markers')
 
