509e6610a094cb087d62b9ea2cc431e20e3ab515
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2023-01-16 16:04:52 -0700

    Processed data sets

---
+++ script/sample_trajectories/TSD.R
@@ -0,0 +1,72 @@
+library(data.table)
+library(ggplot2)
+
+# process min, max times for signal events
+
+DT <- fread("ignore/Phoenix/20221214_IndianSchool.txt")
+DT[, TimeStamp := as.POSIXct(TimeStamp, tz = '', format = '%m-%d-%Y %H:%M:%OS')]
+DT <- DT[between(TimeStamp, '2022-12-14 07:58:00', '2022-12-14 08:20:00'), ]
+
+options(digits.secs = 3L)
+
+dist_3 <- 1219 + 99/2
+dist_7 <- 1219 + 99 + 1509 + 105/2
+dist_15 <- 1219 + 99 + 1509 + 105 + 2281 + 111/2
+dist_19 <- 1219 + 99 + 1509 + 105 + 2281 + 111 + 2515 + 107/2
+
+seq_phase_event <- c(7L, 8L, 9L, 10L, 11L, 1L)
+
+get_min_max <- function(device, int_name, dist){
+    
+    dt <- copy(DT)[DeviceID == device & EventID %in% seq_phase_event & Parameter == 2L, ]
+    
+    minCycleTime <- dt$TimeStamp[min(which(dt$EventID == 7L))]
+    maxCycleTime <- dt$TimeStamp[max(which(dt$EventID == 7L))]
+    
+    dt <- dt[between(TimeStamp, minCycleTime, maxCycleTime), ]
+    dt <- dt[EventID %in% c(8L, 10L, 1L), ]
+    
+    yst <- dt$TimeStamp[dt$EventID == 8L]
+    rst <- dt$TimeStamp[dt$EventID == 10L]
+    gst <- dt$TimeStamp[dt$EventID == 1L]
+    
+    print(yst)
+    cat('\n')
+    print(rst)
+    cat('\n')
+    print(gst)
+    cat('\n')
+    
+    print(c(length(yst), length(rst), length(gst)))
+    
+    data <- data.table(
+        Int = int_name,
+        x = dist,
+        ymin = head(yst, -1L),
+        ymax = rst,
+        rmin = rst,
+        rmax = gst,
+        gmin = gst,
+        gmax = tail(yst, -1L)
+    )
+    
+    return(data)
+}
+
+DT3 <- get_min_max(49L, '3rd', dist_3)
+DT7 <- get_min_max(48L, '7th', dist_7)
+DT15 <- get_min_max(47L, '15th', dist_15)
+DT19 <- get_min_max(46L, '19th', dist_19)
+
+# trajectory data sets
+
+traj1 = fread("script/sample_trajectories/trajectory_c1.txt")
+traj2 = fread("script/sample_trajectories/trajectory_c2.txt")
+traj3 = fread("script/sample_trajectories/trajectory_c3.txt")
+traj4 = fread("script/sample_trajectories/trajectory_c4.txt")
+traj5 = fread("script/sample_trajectories/trajectory_c5.txt")
+
+traj = rbindlist(list(traj1, traj2, traj3, traj4, traj5))
+
+traj[, TimeStamp := as.POSIXct('2022-12-14 08:00:00')]
+traj[, TimeStamp := TimeStamp + t]
